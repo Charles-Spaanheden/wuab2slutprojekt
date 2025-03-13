e2b99e3a7d1f4dbf6c36436579b7d05fdbdabc3f
Charles-Spaanheden <charliespaan@gmail.com>
2025-03-13 09:18:10 +0100

    upd

---
+++ src/routes/timeline/+page.svelte
@@ -0,0 +1,11 @@
+<div class="grid grid-cols-3 grid-rows-5 gap-3 h-screen 
+sm:grid-cols-1">
+    <div class="row-span-full ... bg-blue-400 max-h-screen max-w-full"><img src="src\lib\images\timeline2.svg" alt="timeline" class="sm:w-450 transform rotate-90 sm:rotate-0 sm:scale-100 sm:h-300 scale-600 h-10"></div>
+    <div class="col-span-2 sm:hidden bg-blue-300">1</div>
+    <div class="col-span-2 sm:hidden bg-blue-300">2</div>
+    <div class="col-span-2 sm:hidden bg-blue-300">3</div>
+    <div class="col-span-2 sm:hidden bg-blue-300">4</div>
+    <div class="col-span-2 sm:hidden bg-blue-300">5</div>
+</div>
+
+<div class="sm:grid"></div>
