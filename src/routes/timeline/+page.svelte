17b774664ec64819def8bc20f79284492e1b2293
Charles <charles.spaanheden@elev.ga.ntig.se>
2025-03-28 10:14:53 +0100

    upd

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,11 +1,23 @@
 <script>
     import Article from '../Article.svelte';
+    let isExpanded = false;
 </script>
 
+<div class="grid grid-cols-3 grid-rows-5 gap-3 h-screen sm:grid-cols-1">
+    <button 
+        class="absolute top-4 left-4 bg-blue-500 text-white px-4 py-2 rounded"
+        on:click={() => isExpanded = !isExpanded}>
+        Toggle Layout
+    </button>
 
-<div class="grid grid-cols-3 grid-rows-5 gap-3 h-screen 
-sm:grid-cols-1">
-    <div class="flex justify-center sm:row-span-full ... bg-blue-400 max-h-screen max-w-full"><img src="src\lib\images\timeline2.svg" alt="timeline" class="sm:w-450 transform rotate-90 sm:rotate-0 sm:scale-80 hidden sm:block sm:h-100 h-10"></div>
+    <div 
+        class="flex justify-center bg-blue-400 max-h-screen max-w-full sm:row-span-2"
+        class:sm:row-span-2={!isExpanded}
+        class:sm:row-span-5={isExpanded}>
+        <img src="src\lib\images\timeline2.svg" alt="timeline" 
+            class="sm:w-450 transform rotate-90 sm:rotate-0 sm:scale-80 hidden sm:block sm:h-60 h-10">
+    </div>
+    
     <div class="col-span-2 sm:hidden bg-blue-300">1</div>
     <div class="bg-blue-400 sm:hidden"></div>
     <div class="col-span-2 sm:hidden bg-blue-300">2</div>
@@ -15,8 +27,11 @@ sm:grid-cols-1">
     <div class="col-span-2 sm:hidden bg-blue-300">4</div>
     <div class="bg-blue-400 sm:hidden"></div>
     <div class="col-span-2 sm:hidden bg-blue-300">5</div>
-</div>
 
-<div class="hidden sm:grid">
-    <Article />
+    <div 
+        class="hidden sm:grid"
+        class:row-span-3={!isExpanded}
+        class:sm:hidden={isExpanded}>
+        <Article />
+    </div>
 </div>
